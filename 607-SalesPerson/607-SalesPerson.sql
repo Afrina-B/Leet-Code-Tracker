c17e9dac41523a2fc5c08d912ea8a8a5c668f6c0
Afrina-B <afrinabasheer1111@gmail.com>
2026-07-17 23:56:20 +0530

    Create 607-SalesPerson

---
+++ 607-SalesPerson/607-SalesPerson.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/17/2026, 11:56:21 PM
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT sales_id
+    FROM Orders
+    JOIN Company
+    ON Orders.com_id = Company.com_id
+    WHERE Company.name = 'RED'
+);
