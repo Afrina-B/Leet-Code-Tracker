7de976896764bc0c240f63760008c42eae72c0cb
Afrina-B <afrinabasheer1111@gmail.com>
2026-07-17 23:57:23 +0530

    Create 222-CountCompleteTreeNodes

---
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -0,0 +1,7 @@
+// Last updated: 7/17/2026, 11:57:25 PM
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
