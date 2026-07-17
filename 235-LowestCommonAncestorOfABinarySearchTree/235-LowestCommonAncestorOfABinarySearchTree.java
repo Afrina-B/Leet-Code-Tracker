5037dbb948a94ae4df2ff1bbfe72c3e025a0842b
Afrina-B <afrinabasheer1111@gmail.com>
2026-07-17 23:57:06 +0530

    Create 235-LowestCommonAncestorOfABinarySearchTree

---
+++ 235-LowestCommonAncestorOfABinarySearchTree/235-LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,16 @@
+// Last updated: 7/17/2026, 11:57:08 PM
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+
+        return null;
+    }
+}
