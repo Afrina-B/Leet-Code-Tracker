e2f3952804698b27a966e9edb0d6b06a047fa853
Afrina-B <afrinabasheer1111@gmail.com>
2026-07-17 23:58:01 +0530

    Create 117-PopulatingNextRightPointersInEachNodeIi

---
+++ 117-PopulatingNextRightPointersInEachNodeIi/117-PopulatingNextRightPointersInEachNodeIi.java
@@ -0,0 +1,28 @@
+// Last updated: 7/17/2026, 11:58:03 PM
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node prev = null;
+            for (int i = 0; i < size; i++) {
+                Node curr = queue.poll();
+                if (prev != null) {
+                    prev.next = curr;
+                }
+                prev = curr;
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        return root;
+    }
+}
