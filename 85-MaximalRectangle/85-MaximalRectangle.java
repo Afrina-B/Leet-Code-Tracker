64b60b0d87237a553b844aa145c08206257c7721
Afrina-B <afrinabasheer1111@gmail.com>
2026-07-17 23:58:14 +0530

    Create 85-MaximalRectangle

---
+++ 85-MaximalRectangle/85-MaximalRectangle.java
@@ -0,0 +1,43 @@
+// Last updated: 7/17/2026, 11:58:16 PM
+import java.util.*;
+
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+
+        int[] heights = new int[matrix[0].length];
+        int maxArea = 0;
+
+        for (char[] row : matrix) {
+            for (int i = 0; i < row.length; i++) {
+                if (row[i] == '1')
+                    heights[i]++;
+                else
+                    heights[i] = 0;
+            }
+
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }
+
+    private int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+
+        for (int i = 0; i <= heights.length; i++) {
+            int h = (i == heights.length) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return max;
+    }
+}
