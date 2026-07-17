775096e915bed6b4359c7ef44a93e5dde3395149
Afrina-B <afrinabasheer1111@gmail.com>
2026-07-17 23:56:48 +0530

    Create 295-FindMedianFromDataStream

---
+++ 295-FindMedianFromDataStream/295-FindMedianFromDataStream.java
@@ -0,0 +1,27 @@
+// Last updated: 7/17/2026, 11:56:50 PM
+class MedianFinder {
+    PriorityQueue<Integer> small;
+    PriorityQueue<Integer> large;
+
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        small.offer(num);
+        large.offer(small.poll());
+
+        if (large.size() > small.size()) {
+            small.offer(large.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (small.size() > large.size()) {
+            return small.peek();
+        }
+
+        return (small.peek() + large.peek()) / 2.0;
+    }
+}
